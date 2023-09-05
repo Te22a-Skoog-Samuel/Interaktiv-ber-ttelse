c6a09a22a32f00dd983985c184281d0ded3e4515
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2023-09-05 11:11:34 +0200

    har startat min berättelse

---
+++ Interaktiv berättelse/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Skriv in ditt namn");
+string name = Console.ReadLine().ToLower();
+
+if (name == "skoogen" || name == "samuel")
+{
+    Console.WriteLine("Välkommen");
+
+
+}
+else if (name == "alex")
+{
+    Console.WriteLine("Sveriges stolthet");
+}
+
+else
+{
+    Console.WriteLine("Du är inte välkommen hit");
+}
+
+Console.ReadLine();
