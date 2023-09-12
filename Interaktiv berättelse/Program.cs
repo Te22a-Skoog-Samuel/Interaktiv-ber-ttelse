00c6759ada16c9c7427c6c2b0cf8b243dba57c9b
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2023-09-12 10:33:46 +0200

    dfym

--- Interaktiv berättelse/Program.cs
+++ Interaktiv berättelse/Program.cs
@@ -11,8 +11,8 @@ choice = choice.ToLower();
 
 if (choice == "kvinnan")
 {
-    Console.WriteLine("Du drar bort täcket och du ser att det inte är en kvinna du tittar på utan det är en man");
-    Console.WriteLine("Resultatet blev att du hittade att det var en man som låg bredvid dig men du orsakade också en hel del ljud så nu är mannen vaken.");
+    Console.WriteLine("Du drar bort täcket och du ser att det inte är en kvinna du tittar på utan det är en man.");
+    Console.WriteLine("Du fick svar på din nyfikenhet men du orsakade också en hel del ljud så nu är mannen vaken.");
     Console.WriteLine("Mannen blir lite upprörd och aggresiv så han går till attack och slår sönder dig.");
 
 }
@@ -22,14 +22,33 @@ else if (choice == "rummet")
     Console.WriteLine("Du investigerar rummet och ser att fönstrerna har galler och dörren är låst.");
     Console.WriteLine("Du bestämmer dig för att leta efter nyckeln till dörren.");
     Console.WriteLine("Letar du under sängen eller i kvinnans väska?");
+
+    choice = Console.ReadLine();
+    choice = choice.ToLower();
+
+    if (choice == "sängen")
+    {
+        Console.WriteLine("Du letar och letar men hittar ingenting så du bestämmer dig för att ge upp och leva ut dina sista dar med främlingen.");
+
+    }
+
+    else if (choice == "kvinnans väska")
+    {
+        Console.WriteLine("Du hittar nyckeln i väskan och tar dig ut, ringer polisen och tar kvinnan till domstol.");
+
+    }
+
+    else
+    {
+        Console.WriteLine("Hörredudu, skriv något av alternativen tack.");
+    }
 }
 
-choice = Console.ReadLine();
-choice = choice.ToLower();
-
-if (choice == "sängen")
+else
 {
-    Console.WriteLine("");
-
-
+    Console.WriteLine("Hörredudu, skriv något av alternativen tack.");
 }
+
+
+Console.WriteLine("Tryck ENTER för att avsluta");
+Console.ReadLine();
